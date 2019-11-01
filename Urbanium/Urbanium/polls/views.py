f8e6311af46a4104868acec1f8583f74dcf9c3b1
Thien <q.thien.nguyen@outlook.de>
2019-11-01 11:49:27 +0100

    finish step 5: testing

--- Urbanium/Urbanium/polls/views.py
+++ Urbanium/Urbanium/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.views import generic
 from django.urls import reverse
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -11,12 +12,15 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date*')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'polls/detail.html'
+    template_name = 'polls3/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
